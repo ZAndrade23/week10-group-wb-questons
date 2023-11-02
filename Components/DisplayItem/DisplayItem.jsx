35a91f21e4957d4951a6cb01cf78e97e2f76d54d
Zach Andrade <zach_andrade@yahoo.com>
2023-11-02 11:44:23 -0500

    made files for the whiteboard activity

---
+++ Components/DisplayItem/DisplayItem.jsx
@@ -0,0 +1,9 @@
+function DisplayItem(){
+   
+
+    return (
+        <>
+        
+        </>
+        )
+    }
---
+++ Components/TypeList/TypeList.jsx
@@ -0,0 +1,9 @@
+function TypeList(){
+   
+
+    return (
+        <>
+        
+        </>
+        )
+    }
